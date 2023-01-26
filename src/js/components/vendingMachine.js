d96cc235dc1a285300648083881f57971bc1a649
seoyeon <wjddowkd456@naver.com>
2023-01-26 22:34:06 +0900

    feat: 선택한 음료수 목록 생성 기능 추가

--- src/js/components/vendingMachine.js
+++ src/js/components/vendingMachine.js
@@ -14,6 +14,20 @@ class VendingMachine {
     this.getItemList = getList.querySelector(".get-item-list");
     this.txtTotal = getList.querySelector(".txt-total");
   }
+
+  // 선택한 음료수 목록 생성
+  selectedItemGenerator(target) {
+    const selectedItem = document.createElement("li");
+    selectedItem.dataset.item = target.dataset.item;
+    selectedItem.dataset.price = target.dataset.price;
+    selectedItem.innerHTML = `<div class="select-cola">
+      <img src="/src/img/${target.dataset.img}" alt="" />
+      <strong class="select-name">${target.dataset.item}</strong>
+    </div>
+    <span class="count">1</span>
+      `;
+    this.selectedList.appendChild(selectedItem);
+  }
 }
 
 export default Vendingmachine;
